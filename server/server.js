36d7acadb942206a86982c336ca511799d424b71
hikaru12126 <koide.hikaru2@jrc.co.jp>
2025-06-27 14:01:46 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
-require('dotenv').config(); // これが一番上！
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const chatRouter = require('./routes/chat');
+const chatRouter = require('./routes/chat'); // routes/chat.js という構成
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use('/api', chatRouter);
 
 const PORT = 3001;
